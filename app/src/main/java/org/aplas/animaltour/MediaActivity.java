c11b4a5802d09bb7a47f4997bfcdb661f6fb5ebc
farid-maulana <faridmaulana1921@gmail.com>
2021-11-08 22:13:57 +0700

    Upload project

---
+++ app/src/main/java/org/aplas/animaltour/MediaActivity.java
@@ -0,0 +1,15 @@
+package org.aplas.animaltour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media);
+
+    }
+}
---
+++ app/src/main/java/org/aplas/animaltour/SubInvertActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.animaltour;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubInvertActivity extends AppCompatActivity {
+    public SubInvertActivity() {
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_sub_invert);
+    }
+}
